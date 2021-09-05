0e192f75e2b4448e18e21da892280c93562cf5bb
Oli Brock <kbshields@gmail.com>
2021-09-05 14:25:47 -0400

    (Credits) add simple tween animation to credits on main menu

--- main_menu/MainMenu.gd
+++ main_menu/MainMenu.gd
@@ -1,23 +1,22 @@
 extends Node2D
 
-var transition_layer
-var credits_text
-var non_credits
+onready var transition_layer = $TransitionLayer
+onready var credits_text = $CreditsText
+onready var non_credits = $CreditsTip
+
+onready var tween = Tween.new()
+
 func _ready():
 	yield(get_tree(), "idle_frame")
 	var tree = get_tree()
-
-	credits_text = tree.current_scene.get_node("CreditsText")
-	credits_text.set_visible(false);
-	non_credits = tree.current_scene.get_node("HideForCredits")
-	transition_layer = tree.current_scene.get_node("TransitionLayer")
-	transition_layer.connect("fade_out_complete", self, "switch_scenes")
+	add_child(tween)
+	if not transition_layer.is_connected("fade_out_complete", self, "switch_scenes"):
+		assert(transition_layer.connect("fade_out_complete", self, "switch_scenes")== OK)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("credits"):
-		credits_text.set_visible(credits_text.is_visible()==false);
-		non_credits.set_visible(credits_text.is_visible()==false);
+		_toggle_credits()
 	if Input.is_action_just_pressed("ui_accept"):
 		transition_layer.fade_out()
 #	pass
@@ -25,4 +24,16 @@ func _process(delta):
 func switch_scenes():
 #	get_tree().change_scene("res://Levels/TutorialLevel.tscn")
 	Global.goto_scene(Global.level_list[Global.current_level])
-	
+
+func _toggle_credits():
+	if tween.is_active():
+		tween.stop()
+	if credits_text.rect_position.y > 150:		
+		non_credits.set_visible(false)
+		tween.interpolate_property(credits_text, "rect_position:y", 160, 0, 3, Tween.TRANS_EXPO, Tween.EASE_OUT)
+		tween.start()
+	else:
+		non_credits.set_visible(true)
+		tween.interpolate_property(credits_text, "rect_position:y", credits_text.rect_position.y, 160, .5, Tween.TRANS_EXPO, Tween.EASE_OUT)
+		tween.start()
+
