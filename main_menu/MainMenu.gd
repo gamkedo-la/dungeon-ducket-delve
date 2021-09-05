c1bbfe4820443596ce43c25772234727f6a2475b
Gamkedo <chris@gamkedo.com>
2021-09-05 07:49:57 -0700

    Working on a rough but functional (possibly placeholder) credits integration, adding hide in next commit along with improved visibility

--- main_menu/MainMenu.gd
+++ main_menu/MainMenu.gd
@@ -1,15 +1,20 @@
 extends Node2D
 
 var transition_layer
+var credits_text
 func _ready():
 	yield(get_tree(), "idle_frame")
 	var tree = get_tree()
 
+	credits_text = tree.current_scene.get_node("CreditsText")
+	credits_text.hide();
 	transition_layer = tree.current_scene.get_node("TransitionLayer")
 	transition_layer.connect("fade_out_complete", self, "switch_scenes")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("credits"):
+		credits_text.show();
 	if Input.is_action_just_pressed("ui_accept"):
 		transition_layer.fade_out()
 #	pass
