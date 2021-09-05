825c1728cccdbf0f4f0814f4721acc3ce46394eb
Gamkedo <chris@gamkedo.com>
2021-09-05 08:07:40 -0700

    Title details now hide when credits are displayed, updated tip to mention space to start

--- main_menu/MainMenu.gd
+++ main_menu/MainMenu.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 var transition_layer
 var credits_text
+var non_credits
 func _ready():
 	yield(get_tree(), "idle_frame")
 	var tree = get_tree()
 
 	credits_text = tree.current_scene.get_node("CreditsText")
 	credits_text.set_visible(false);
+	non_credits = tree.current_scene.get_node("HideForCredits")
 	transition_layer = tree.current_scene.get_node("TransitionLayer")
 	transition_layer.connect("fade_out_complete", self, "switch_scenes")
 
@@ -15,6 +17,7 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("credits"):
 		credits_text.set_visible(credits_text.is_visible()==false);
+		non_credits.set_visible(credits_text.is_visible()==false);
 	if Input.is_action_just_pressed("ui_accept"):
 		transition_layer.fade_out()
 #	pass
